99b0a1c0ded99053dfa7d8506ed503e738eec5ea
Mk <Mk@Martin-K---Macbook-Pro.local>
2025-12-15 14:37:17 +0100

    Sjednoť odkazy na profily na /u/slug

--- app/profile/[slug]/page.tsx
+++ app/profile/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function LegacyProfileSlugRedirect({ params }: { params: { slug: string } }) {
-  return redirect(`/artist/${params.slug}`);
+  return redirect(`/u/${params.slug}`);
 }
