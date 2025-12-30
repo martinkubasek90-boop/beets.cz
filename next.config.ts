b7ca0341d1151d881e5c12f733b8a133d7776f30
Mk <Mk@Martin-K---Macbook-Pro.local>
2025-12-30 11:15:57 +0100

    Include ffmpeg binary in trace

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
     // Nutí Next najít kořen v tomto projektu (kvůli více lockfile v nadřazených složkách)
     root: __dirname,
   },
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/konvertor': ['node_modules/ffmpeg-static/ffmpeg'],
+    },
+  },
 };
 
 export default nextConfig;
