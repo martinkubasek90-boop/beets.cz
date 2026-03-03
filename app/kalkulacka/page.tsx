f21195427e7e3e1bb5827376b831b16590896d5d
Mk <Mk@Martin-K---Macbook-Pro.local>
2026-03-03 19:06:09 +0100

    remove header from kalkulacka page

--- app/kalkulacka/page.tsx
+++ app/kalkulacka/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import { MainNav } from '@/components/main-nav';
 import BessCalculator from '@/components/bess/BessCalculator';
 
 export default function KalkulackaPage() {
   return (
     <div className="min-h-screen bg-slate-950 text-white">
-      <MainNav />
       <BessCalculator />
     </div>
   );
