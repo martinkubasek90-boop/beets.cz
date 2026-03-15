072db6f861976ab23afb934ec747350893a7acb1
Mk <Mk@Martin-K---Macbook-Pro.local>
2026-03-15 16:30:28 +0100

    Fix speech recognition cast for build

--- components/memodo/header-search.tsx
+++ components/memodo/header-search.tsx
@@ -457,7 +457,7 @@ export function MemodoHeaderSearch() {
       return;
     }
 
-    const recognition = new Recognition() as SpeechRecognitionLike;
+    const recognition = new Recognition() as unknown as SpeechRecognitionLike;
     recognition.lang = "cs-CZ";
     recognition.interimResults = true;
     recognition.maxAlternatives = 1;
